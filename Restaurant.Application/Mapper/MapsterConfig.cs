55edb60e95467da225ec28797a84417ad846bee8
Ahmed Saber <154698661+ahmeddsaber@users.noreply.github.com>
2026-01-19 11:23:05 +0200

    Add Swagger Configuration

---
+++ Restaurant.Application/Mapper/MapsterConfig.cs
@@ -0,0 +1,24 @@
+﻿using Mapster;
+using Restaurant.Application.DTOS.MenuItem;
+using Restaurant.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Application.Mapper
+{
+    public static class MapsterConfig
+    {
+
+        public static void Configure()
+        {
+            TypeAdapterConfig<MenuItem, MenuItemDto>.NewConfig()
+           .Map(dest => dest.NameEn, src => src.NameEn)
+            .Map(dest => dest.NameAr, src => src.NameAr)
+            .TwoWays();
+
+        }
+    }
+}
